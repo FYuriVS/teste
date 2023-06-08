f96c38e513b24e2b7ff29c59c16b82237dbba714
Yuri <fyurivs@outlook.com>
2023-06-08 14:39:33 -0300

    Adicionado FAB(botão flutuante)

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,10 +1,19 @@
-import { ScrollView, StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
 
 const StatusBarHeight = StatusBar.currentHeight;
 
 export default function Profile() {
-  const cardsData = [
+  const [clientsData, setClientsData] = useState([
     { id: 1, title: "Card 1", description: "Description 1" },
     { id: 2, title: "Card 2", description: "Description 2" },
     { id: 3, title: "Card 3", description: "Description 3" },
@@ -19,11 +28,12 @@ export default function Profile() {
     { id: 12, title: "Card 12", description: "Description 12" },
     { id: 13, title: "Card 13", description: "Description 13" },
     { id: 14, title: "Card 14", description: "Description 14" },
-  ];
+    { id: 15, title: "Card 15", description: "Description 15" },
+  ]);
 
   const rows = [];
-  for (let i = 0; i < cardsData.length; i += 2) {
-    const row = cardsData.slice(i, i + 2);
+  for (let i = 0; i < clientsData.length; i += 2) {
+    const row = clientsData.slice(i, i + 2);
     rows.push(row);
   }
 
@@ -34,7 +44,11 @@ export default function Profile() {
           {rows.map((row, rowIndex) => (
             <View style={styles.rowSection} key={rowIndex}>
               {row.map((card) => (
-                <View style={[styles.card, styles.shadowProp]} key={card.id}>
+                <View
+                  style={[styles.card, styles.shadowProp]}
+                  key={card.id}
+                  //   onClick={() => {}}
+                >
                   <Text style={styles.cardTitle}>{card.title}</Text>
                   <Text style={styles.cardDescription}>{card.description}</Text>
                 </View>
@@ -43,6 +57,21 @@ export default function Profile() {
           ))}
         </View>
       </ScrollView>
+      <TouchableOpacity
+        style={styles.fab}
+        onPress={() => {
+          setClientsData([
+            ...clientsData,
+            {
+              id: "55",
+              title: "Card 55",
+              description: "Description 55",
+            },
+          ]);
+        }}
+      >
+        <AntDesign name="plus" size={24} color="white" />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -90,5 +119,17 @@ const styles = StyleSheet.create({
     shadowOffset: { largura: -2, altura: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
+  },
+  fab: {
+    position: "absolute",
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: "#2196F3", // Cor de fundo do FAB
+    alignItems: "center",
+    justifyContent: "center",
+    right: 16,
+    bottom: 56,
+    elevation: 8, // Sombra (opcional, depende da plataforma)
   },
 });
